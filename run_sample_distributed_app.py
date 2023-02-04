a5cba1c39569d090feaf74600bea1935a945c315
C <3979063+craig8@users.noreply.github.com>
2023-02-03 21:01:58 -0800

    Added proper logging for sample dist app

--- run_sample_distributed_app.py
+++ run_sample_distributed_app.py
@@ -24,6 +24,8 @@ agents_mod.set_cim_profile(cim_profile)
 
 cim = agents_mod.cim
 
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('goss').setLevel(logging.ERROR)
 logging.getLogger('stomp.py').setLevel(logging.ERROR)
 
 _log = logging.getLogger(__name__)
@@ -32,19 +34,20 @@ _log = logging.getLogger(__name__)
 class SampleCoordinatingAgent(CoordinatingAgent):
 
     def __init__(self, feeder_id, system_message_bus_def, simulation_id=None):
-        super(SampleCoordinatingAgent, self).__init__(feeder_id, system_message_bus_def, simulation_id)
+        super().__init__(feeder_id, system_message_bus_def, simulation_id)
 
 
 class SampleFeederAgent(FeederAgent):
 
     def __init__(self, upstream_message_bus_def: MessageBusDefinition, downstream_message_bus_def: MessageBusDefinition,
                  feeder_dict: Dict = None, simulation_id: str = None):
-        super(SampleFeederAgent, self).__init__(upstream_message_bus_def, downstream_message_bus_def,
+        super().__init__(upstream_message_bus_def, downstream_message_bus_def,
                                                 feeder_dict, simulation_id)
 
         
     #TODO remove first four
-    def on_measurement(self, peer, sender, bus, topic, headers, message):
+    def on_measurement(self, headers: Dict, message) -> None:
+        _log.debug(f"measurement: {self.__class__.__name__}.{headers.get('destination')}")
         with open("feeder.txt", "a") as fp:
             fp.write(json.dumps(message))
         #print(message)
@@ -54,10 +57,11 @@ class SampleSwitchAreaAgent(SwitchAreaAgent):
 
     def __init__(self, upstream_message_bus_def: MessageBusDefinition, downstream_message_bus_def: MessageBusDefinition,
                  switch_area_dict: Dict = None, simulation_id: str = None):
-        super(SampleSwitchAreaAgent, self).__init__(upstream_message_bus_def, downstream_message_bus_def,
+        super().__init__(upstream_message_bus_def, downstream_message_bus_def,
                                                     switch_area_dict, simulation_id)
 
     def on_measurement(self, peer, sender, bus, topic, headers, message):
+        _log.debug(f"measurement: {self.__class__.__name__}.{headers.get('destination')}")
         with open("switch_area.txt", "a") as fp:
             fp.write(json.dumps(message))
         #print(message)
@@ -67,18 +71,15 @@ class SampleSecondaryAreaAgent(SecondaryAreaAgent):
 
     def __init__(self, upstream_message_bus_def: MessageBusDefinition, downstream_message_bus_def: MessageBusDefinition,
                  secondary_area_dict: Dict = None, simulation_id: str = None):
-        super(SampleSecondaryAreaAgent, self).__init__(upstream_message_bus_def, downstream_message_bus_def,
+        super().__init__(upstream_message_bus_def, downstream_message_bus_def,
                                                        secondary_area_dict, simulation_id)
 
         
 
     def on_measurement(self, peer, sender, bus, topic, headers, message):
+        _log.debug(f"measurement: {self.__class__.__name__}.{headers.get('destination')}")
         with open("secondary.txt", "a") as fp:
-            if "_4c491539-dfc1-4fda-9841-3bf10348e2fa" in json.dumps(message):
-                print("Woot found it!")
-                sys.exit()
             fp.write(json.dumps(message))
-        #print(message)
 
 
 def overwrite_parameters(yaml_path: str, feeder_id: str) -> MessageBusDefinition:
