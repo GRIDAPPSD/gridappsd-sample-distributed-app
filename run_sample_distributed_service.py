56d227b5b419e94035be05c47dd50344416a6177
poorva1209 <poorva1209@gmail.com>
2023-03-27 11:27:02 -0700

    implemented neighboring agent discovery and agent to agent communication

--- run_sample_distributed_service.py
+++ run_sample_distributed_service.py
@@ -14,13 +14,9 @@ from cimlab.data_profile import CIM_PROFILE
 from pathlib import Path
 
 import gridappsd.field_interface.agents.agents as agents_mod
-import gridappsd.topics as t 
 from gridappsd.field_interface.agents import CoordinatingAgent, FeederAgent, SwitchAreaAgent, SecondaryAreaAgent
 from gridappsd.field_interface.context import LocalContext
 from gridappsd.field_interface.interfaces import MessageBusDefinition
-
-import sample_queries as example
-from idlelib.rpc import request_queue
 
 cim_profile = CIM_PROFILE.RC4_2021.value
 
@@ -73,7 +69,7 @@ class SampleFeederAgent(FeederAgent):
         
         reply_to = headers['reply-to']
         
-        message_bus.send(reply_to,'this is a reponse')
+        message_bus.send(reply_to,{'test_response':'this is a test response'})
 
 
 class SampleSwitchAreaAgent(SwitchAreaAgent):
@@ -96,6 +92,13 @@ class SampleSwitchAreaAgent(SwitchAreaAgent):
             fp.write(json.dumps(message))
         #print(message)
         
+    def on_request(self, message_bus, headers: Dict, message):
+        print(f"Received request: {message}")
+        
+        reply_to = headers['reply-to']
+        
+        message_bus.send(reply_to,{'test_response':'this is a test response'})
+        
     
 class SampleSecondaryAreaAgent(SecondaryAreaAgent):
 
@@ -115,6 +118,13 @@ class SampleSecondaryAreaAgent(SecondaryAreaAgent):
         _log.debug(f"measurement: {self.__class__.__name__}.{headers.get('destination')}")
         with open("secondary.txt", "a") as fp:
             fp.write(json.dumps(message))
+            
+    def on_request(self, message_bus, headers: Dict, message):
+        print(f"Received request: {message}")
+        
+        reply_to = headers['reply-to']
+        
+        message_bus.send(reply_to,{'test_response':'this is a test response'})
 
 
 def overwrite_parameters(yaml_path: str, feeder_id: str) -> MessageBusDefinition:
@@ -136,8 +146,8 @@ def overwrite_parameters(yaml_path: str, feeder_id: str) -> MessageBusDefinition
 def _main():
 
     agent_config = {
-                    "app_id":"sample_app",
-                    "description":"This is a GridAPPS-D sample distribution application agent"
+                    "app_id":"local_service",
+                    "description":"This is a GridAPPS-D sample distributed service agent"
                 }
     
     feeder_path = Path("simulation.feeder.txt")
@@ -168,10 +178,6 @@ def _main():
     coordinating_agent.spawn_distributed_agent(feeder_agent)
     
     
-    # Get all the attributes of the equipments in the feder area from the model 
-    #TODO: Uncomment when feeder attributed query working in gridappsd
-    #print(feeder_agent.feeder_area.get_all_attributes(cim.PowerTransformer))
-    
     # create switch area distributed agents
     switch_areas = context['data']['switch_areas']
     for sw_index, switch_area in enumerate(switch_areas):
@@ -184,44 +190,7 @@ def _main():
                                                   simulation_id)
         coordinating_agent.spawn_distributed_agent(switch_area_agent)
         
-        # Get all the attributes of the equipments in the switch area from the model 
-        
-        # EXAMPLE 1 - Get phase, bus info about ACLineSegments
-        example.get_lines_buses(switch_area_agent.switch_area)
-        
-        # EXAMPLE 2 - Get all line impedance data
-        example.get_line_impedances(switch_area_agent.switch_area)
-        
-        # EXAMPLE 3 - Sort all line impedance by line phase:
-        example.sort_impedance_by_line(switch_area_agent.switch_area)
-        
-        # Example 4 - Sort all lines by impedance
-        example.sort_line_by_impedance(switch_area_agent.switch_area)
-        
-        # Example 5 - Get TransformerTank impedances
-        example.get_tank_impedances(switch_area_agent.switch_area)
-        
-        # Example 6 - Get inverter buses and phases
-        example.get_inverter_buses(switch_area_agent.switch_area)
-        
-        # Example 7 - Get load buses and phases
-        example.get_load_buses(switch_area_agent.switch_area)
-        
-        
-        service_agent_id = None
-        response = LocalContext.get_agents(switch_area_agent.downstream_message_bus)
-        print(f"Agents: {response}")
-        for agent_id, agent_details in response.items():
-            if agent_details['app_id'] == 'local_service':
-                service_agent_id = agent_id
-                
-        request_queue = t.field_agent_request_queue(switch_area_agent.downstream_message_bus.id, service_agent_id)
-        print(request_queue)
-        response = switch_area_agent.downstream_message_bus.get_response(request_queue,{'test':'test request'})
-        print(f'Response from service agent: {response}')
-
-
-        # create secondary area distributed agents
+                # create secondary area distributed agents
         for sec_index, secondary_area in enumerate(switch_area['secondary_areas']):
             secondary_area_message_bus_def = overwrite_parameters(f"config_files_simulated/secondary_area_message_bus_{sw_index}_{sec_index}.yml", feeder_id)
             print("Creating secondary area agent " + str(secondary_area['message_bus_id']))
@@ -232,12 +201,6 @@ def _main():
                                                             simulation_id)
             if len(secondary_area_agent.secondary_area.addressable_equipment) > 1:
                 coordinating_agent.spawn_distributed_agent(secondary_area_agent)
-                
-                # Example 6 - Get inverter buses and phases
-                example.get_inverter_buses(secondary_area_agent.secondary_area)
-
-                # Example 7 - Get load buses and phases
-                example.get_load_buses(secondary_area_agent.secondary_area)
 
     '''
     # Publish device data
